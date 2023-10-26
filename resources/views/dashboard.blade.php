5e45a0200df8f09a85d454879834456b7753dcf1
boburasrorov <boburasrorov2005@gmail.com>
2023-10-27 03:59:38 +0500

    Client and Mangager sites are created but does not work

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,8 +11,110 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @if(auth()->user()->name == 'Manager')
                         "You are manager"
+                        <!-- component -->
+                        <!-- This is an example component -->
+                        <div class='py-5'>  <div class="rounded-xl border p-5 shadow-md w-9/12 bg-dark">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+                                        <div class="text-lg font-bold text-dark-700 ">Joe Smith</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button class="rounded-2xl border bg-dark-100 px-3 py-1  text-xs font-bold">ID:</button>
+                                        <div class="text-xs  ">Created at: </div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4 mb-6">
+                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
+                                    <div class="text-sm ">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa</div>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        <p >Email</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='py-5'>  <div class="rounded-xl border p-5 shadow-md w-9/12 bg-dark">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+                                        <div class="text-lg font-bold text-dark-700 ">Joe Smith</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button class="rounded-2xl border bg-dark-100 px-3 py-1  text-xs font-bold">ID:</button>
+                                        <div class="text-xs  ">Created at: </div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4 mb-6">
+                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
+                                    <div class="text-sm ">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa</div>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        <p >Email</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='py-5'>  <div class="rounded-xl border p-5 shadow-md w-9/12 bg-dark">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+                                        <div class="text-lg font-bold text-dark-700 ">Joe Smith</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button class="rounded-2xl border bg-dark-100 px-3 py-1  text-xs font-bold">ID:</button>
+                                        <div class="text-xs  ">Created at: </div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4 mb-6">
+                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
+                                    <div class="text-sm ">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa</div>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        <p >Email</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     @elseif(auth()->user()->name == 'Client')
                         "You are Client"
+                        <!-- component -->
+                        <div class='flex items-center justify-center min-h-screen from-teal-100 via-teal-300 to-teal-500 bg-dark'>
+                            <div class='w-full max-w-lg px-10 py-8 mx-auto bg-dark rounded-lg shadow-xl'>
+                                <div class='max-w-md mx-auto space-y-6'>
+
+                                    <form action="">
+                                        <h2 class="text-2xl font-bold ">Submit your application</h2>
+                                        <p class="my-4 opacity-70">Adipisicing elit. Quibusdam magnam sed ipsam deleniti debitis laboriosam praesentium dolorum doloremque beata.</p>
+                                        <hr class="my-6">
+                                        <label class="uppercase text-sm font-bold opacity-70">Name</label>
+                                        <input type="text" class="bg-black p-3 mt-2 mb-4 w-full  rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                                        <label class="uppercase  text-sm font-bold opacity-70">Email</label>
+                                        <input type="text" class="p-3  mt-2 mb-4 w-full bg-black rounded">
+                                        <label class="uppercase text-sm font-bold opacity-70">Language</label>
+                                        <input type="file" class="p-3  mt-2 mb-4 w-full bg-black rounded">
+
+                                        <div class="my-2 font-medium opacity-70">
+                                            <input type="checkbox">
+                                            Subscribe and follow company updates.
+                                        </div>
+                                        <input type="submit" class="py-3 px-6 my-2 bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300" value="Send">
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
                     @endif
 
                 </div>
